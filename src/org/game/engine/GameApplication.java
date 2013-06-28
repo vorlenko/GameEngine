a81ecaf72ac14d20db326de38e1312fe43aafdee
bob <bob@bob-Aspire-5739G>
2013-06-28 23:52:00 +0600

    Import

--- src/org/game/engine/GameApplication.java
+++ src/org/game/engine/GameApplication.java
@@ -4,6 +4,7 @@
  */
 package org.game.engine;
 
+import java.awt.Rectangle;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
@@ -19,7 +20,9 @@ public class GameApplication {
 			@Override
 			public void run() {
 				JFrame frame = new JFrame(game.getTitle());
-				frame.setSize(game.getWidth(), game.getHeight());
+				//frame.setSize(game.getWidth(), game.getHeight());
+                                frame.setBounds(new Rectangle(game.getWidth()+14, 
+                                        game.getHeight()+29));//This need for my computer.
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				GameCanvas canvas = new GameCanvas();
 				canvas.setGame(game);
